752aff7502fe6df0147c59f4e50c1f818d3bb2be
Junior Vieira <junior.vieira.1990@gmail.com>
2022-09-17 21:08:41 -0300

    :tada: Commit inicial

---
+++ src/main/java/dev/paulovieira/springuninter/SpringuninterApplication.java
@@ -0,0 +1,13 @@
+package dev.paulovieira.springuninter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuninterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringuninterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/paulovieira/springuninter/SpringuninterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paulovieira.springuninter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringuninterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
