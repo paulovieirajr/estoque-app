3adc431f9e1097a96664b00896edc508008eb61e
Junior Vieira <junior.vieira.1990@gmail.com>
2022-09-24 19:29:36 -0300

    :test_tube:[test]: Refeito os testes da Classe de Servico Cliente

--- src/test/java/dev/paulovieira/springuninter/services/impl/ClienteServiceImplTest.java
+++ src/test/java/dev/paulovieira/springuninter/services/impl/ClienteServiceImplTest.java
@@ -3,21 +3,21 @@ package dev.paulovieira.springuninter.services.impl;
 import dev.paulovieira.springuninter.models.Cliente;
 import dev.paulovieira.springuninter.models.enums.Sexo;
 import dev.paulovieira.springuninter.repositories.ClienteRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class ClienteServiceImplTest {
 
@@ -36,28 +36,44 @@ class ClienteServiceImplTest {
     @Mock
     private ClienteRepository repository;
 
+    private Cliente cliente;
+
     @BeforeEach
     void setUp() {
-        // Inicializa o mockito
-        MockitoAnnotations.initMocks(this);
+        openMocks(this);
+        startClient();
     }
 
     @Test
-    void encontrarPorId() {
-        when(repository.findById(any())).thenReturn(Optional.of(new Cliente()));
+    void deveRetornarUmClienteQuandoBuscarPorId() {
+        when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(cliente));
 
-        Cliente cliente = service.encontrarPorId(1L);
+        Cliente clienteEncontrado = service.encontrarPorId(1L);
 
-        assertNotNull(cliente);
+        assertNotNull(clienteEncontrado);
+        assertEquals(NOME, clienteEncontrado.getNome());
+        assertEquals(CPF, clienteEncontrado.getCpf());
+        assertEquals(NASCIMENTO, clienteEncontrado.getDataNascimento());
+        assertEquals(EMAIL, clienteEncontrado.getEmail());
+        assertEquals(TELEFONE, clienteEncontrado.getTelefone());
+        assertEquals(CELULAR, clienteEncontrado.getCelular());
+        assertEquals(SEXO, clienteEncontrado.getSexo());
     }
 
     @Test
-    void encontrarTodos() {
-        when(repository.findAll()).thenReturn(List.of(new Cliente()));
+    void deveRetornarUmaListaDeClientes() {
+        when(repository.findAll()).thenReturn(List.of(cliente));
 
         List<Cliente> clientes = service.encontrarTodos();
 
         assertEquals(1, clientes.size());
+        assertEquals(NOME, clientes.get(0).getNome());
+        assertEquals(CPF, clientes.get(0).getCpf());
+        assertEquals(NASCIMENTO, clientes.get(0).getDataNascimento());
+        assertEquals(EMAIL, clientes.get(0).getEmail());
+        assertEquals(TELEFONE, clientes.get(0).getTelefone());
+        assertEquals(CELULAR, clientes.get(0).getCelular());
+        assertEquals(SEXO, clientes.get(0).getSexo());
     }
 
     @Test
@@ -80,7 +96,7 @@ class ClienteServiceImplTest {
 
     @Test
     void excluir() {
-        when(repository.findById(any())).thenReturn(Optional.of(new Cliente()));
+        when(repository.findById(any())).thenReturn(Optional.of(cliente));
         doNothing().when(repository).deleteById(any());
 
         service.excluir(1L);
@@ -90,8 +106,8 @@ class ClienteServiceImplTest {
 
     @Test
     void atualizar() {
-        when(repository.findById(any())).thenReturn(Optional.of(new Cliente()));
-        when(repository.save(any(Cliente.class))).thenReturn(new Cliente());
+        when(repository.findById(any())).thenReturn(Optional.of(cliente));
+        when(repository.save(any(Cliente.class))).thenReturn(cliente);
 
         Cliente cliente = new Cliente();
         cliente.setNome(NOME);
@@ -109,8 +125,8 @@ class ClienteServiceImplTest {
 
     @Test
     void inativar() {
-        when(repository.findById(any())).thenReturn(Optional.of(new Cliente()));
-        when(repository.save(any(Cliente.class))).thenReturn(new Cliente());
+        when(repository.findById(any())).thenReturn(Optional.of(cliente));
+        when(repository.save(any(Cliente.class))).thenReturn(cliente);
 
         service.inativar(1L);
 
@@ -119,11 +135,23 @@ class ClienteServiceImplTest {
 
     @Test
     void ativar() {
-        when(repository.findById(any())).thenReturn(Optional.of(new Cliente()));
-        when(repository.save(any(Cliente.class))).thenReturn(new Cliente());
+        when(repository.findById(any())).thenReturn(Optional.of(cliente));
+        when(repository.save(any(Cliente.class))).thenReturn(cliente);
 
         service.ativar(1L);
 
         verify(repository, times(1)).save(any());
+    }
+
+    private void startClient() {
+        cliente = new Cliente();
+        cliente.setId(1L);
+        cliente.setNome(NOME);
+        cliente.setCpf(CPF);
+        cliente.setDataNascimento(NASCIMENTO);
+        cliente.setEmail(EMAIL);
+        cliente.setTelefone(TELEFONE);
+        cliente.setCelular(CELULAR);
+        cliente.setSexo(SEXO);
     }
 }
