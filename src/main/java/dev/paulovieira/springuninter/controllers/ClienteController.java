9eb2900b428d3e36edcc40874ff15a34eba513bc
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-01 15:27:10 -0300

    :sparkles:[feat]: Implementado 4 novos metodos.

--- src/main/java/dev/paulovieira/springuninter/controllers/ClienteController.java
+++ src/main/java/dev/paulovieira/springuninter/controllers/ClienteController.java
@@ -4,10 +4,7 @@ import dev.paulovieira.springuninter.models.Cliente;
 import dev.paulovieira.springuninter.services.impl.ClienteServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -35,5 +32,33 @@ public class ClienteController {
     @GetMapping("/salvocomsucesso")
     public ModelAndView salvarComSucesso(ModelMap model){
         return new ModelAndView("cliente/salvocomsucesso", model);
+    }
+
+    @GetMapping
+    public ModelAndView obterTodosClientes(ModelMap model){
+        model.addAttribute("clientes", clienteService.encontrarTodos());
+        return new ModelAndView("cliente/lista", model);
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView editarCliente(@PathVariable Long id, ModelMap model){
+        model.addAttribute("cliente", clienteService.encontrarPorId(id));
+        return new ModelAndView("cliente/formulario", model);
+    }
+
+    @GetMapping("inativar/{id}")
+    public String inativarCliente(@PathVariable Long id){
+        Cliente cliente = clienteService.encontrarPorId(id);
+        cliente.setAtivo(false);
+        clienteService.salvar(cliente);
+        return "redirect:/clientes";
+    }
+
+    @GetMapping("ativar/{id}")
+    public String ativarCliente(@PathVariable Long id){
+        Cliente cliente = clienteService.encontrarPorId(id);
+        cliente.setAtivo(true);
+        clienteService.salvar(cliente);
+        return "redirect:/clientes";
     }
 }
