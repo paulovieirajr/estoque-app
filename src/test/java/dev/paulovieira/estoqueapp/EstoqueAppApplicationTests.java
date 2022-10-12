94bcfa0d258f484a29a7484027aef3e32aa1f4d0
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-12 15:20:01 -0300

    :truck: [chore]: Renomeada a aplicação

--- src/test/java/dev/paulovieira/estoqueapp/EstoqueAppApplicationTests.java
+++ src/test/java/dev/paulovieira/estoqueapp/EstoqueAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringuninterApplicationTests {
+class EstoqueAppApplicationTests {
 
     @Test
     void contextLoads() {
